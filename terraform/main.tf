bac964dd6576db26578a9e9464586a9331597bb5
ayubfarahh <ayubfarahh@gmail.com>
2025-09-06 21:13:59 +0100

    Working on ALB security groups

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,12 @@
 module "vpc" {
     source = "./modules/vpc"
+
   
 }
 
 module "alb" {
     source = "./modules/alb"
+    pub_subnets = module.vpc.pub_subnets
+    vpc_id = module.vpc.vpc_id
   
 }
---
+++ terraform/modules/vpc/outputs.tf
@@ -0,0 +1,13 @@
+output "pub_subnets" {
+    value = [
+        aws_subnet.pub_sub1.id,
+        aws_subnet.pub_sub2.id
+
+    ]
+  
+}
+
+output "vpc_id" {
+    value = aws_vpc.vpc.id
+  
+}
