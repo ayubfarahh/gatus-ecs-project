1aecf42e8b74af8135c9b799c5ca035125d53005
ayubfarahh <ayubfarahh@gmail.com>
2025-09-10 02:04:18 +0100

    created acm modiule to request certificate

--- terraform/modules/route53/main.tf
+++ terraform/modules/route53/main.tf
@@ -1,11 +1,11 @@
 resource "aws_route53_zone" "zone" {
-    name = "ayubs.uk"
+    name = "coderco.ayubs.uk"
   
 }
 
 resource "aws_route53_record" "root" {
     zone_id = aws_route53_zone.zone.zone_id
-    name = "test.ayubs.uk"
+    name = "dev.coderco.ayubs.uk"
     type = "A"
     ttl = 300
 
